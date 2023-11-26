b56b5d6f7ea6f85b348e61ef094691ec9d7ccb9a
thanyaresbenth <benthanyares@gmail.com>
2023-11-26 23:38:22 +0700

    css page create item

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {CreateItemComponent} from "./create-item/create-item.component";
+import {HomeComponent} from "./home/home.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path : "create-item",component :CreateItemComponent},
+  { path : "" , component :HomeComponent}
+
+
+];
---
+++ src/app/create-item/create-item.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-create-item',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './create-item.component.html',
+  styleUrl: './create-item.component.css'
+})
+export class CreateItemComponent {
+
+}
